b30762b439b705ecefda21edcba10c6845d5ec4f
Razvan <razvan.bangu@gmail.com>
2014-08-01 16:08:49 -0400

    Trying suff

--- src/araxxor/HighlightPathToLightTask.java
+++ src/araxxor/HighlightPathToLightTask.java
@@ -12,7 +12,7 @@ import org.powerbot.script.rt6.LocalPath;
 public class HighlightPathToLightTask extends Task<ClientContext> {
 	private final int groundLightId = 91665;
 	
-	private LocalPath pathToLight = null;
+	private Tile lightTile = null;
 
 	public HighlightPathToLightTask(ClientContext ctx) {
 		super(ctx);
@@ -21,10 +21,9 @@ public class HighlightPathToLightTask extends Task<ClientContext> {
 	@Override
 	public boolean activate() {
 		if(!ctx.objects.select().id(91665).isEmpty()) { /* The light is visible somewhere */
-			System.out.println("Found the light!");
 			return true;
 		} else { /* There's no light; let's clear our path so we don't try to draw stuff */
-			this.pathToLight = null;
+			this.lightTile = null;
 			return false;
 		}
 	}
@@ -33,21 +32,16 @@ public class HighlightPathToLightTask extends Task<ClientContext> {
 	public void execute() {
 		/* Set up our path */
 		GameObject light = ctx.objects.select().id(this.groundLightId).poll();
-		this.pathToLight = ctx.movement.findPath(light);
+		this.lightTile = light.tile();
 	}
 
 	@Override
 	public void repaint(Graphics g) {
 		g.setColor(Color.CYAN);
 		
-		if(this.pathToLight != null) {
-			Tile nextTile = this.pathToLight.next();
-			while(nextTile != this.pathToLight.end()) { /* Loop through our path until we get to the end */
-				Point mapPoint = nextTile.matrix(ctx).mapPoint();
-				g.drawPolygon(nextTile.matrix(ctx).bounds()); // Draw the polygon on screen
-				g.drawRect((int)mapPoint.getX() - 2, (int)mapPoint.getY() - 2, 4, 4); // Draw the point on the map
-				nextTile = this.pathToLight.next();
-			}
+		if(this.lightTile != null) {
+			Point mapPoint = lightTile.matrix(ctx).mapPoint();
+			g.fillRect((int)mapPoint.getX() - 2, (int)mapPoint.getY() - 2, 4, 4); // Draw the point on the map
 		}
 	}
 
