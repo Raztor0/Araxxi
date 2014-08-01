550a0311df8ae7dd8c4cc7b012e985fca16af7b9
Razvan <razvan.bangu@gmail.com>
2014-08-01 15:23:01 -0400

    Now it will draw the path to the light.

--- src/araxxor/HighlightPathToLightTask.java
+++ src/araxxor/HighlightPathToLightTask.java
@@ -20,37 +20,35 @@ public class HighlightPathToLightTask extends Task<ClientContext> {
 
 	@Override
 	public boolean activate() {
-//		if(!ctx.objects.select().id(91665).isEmpty()) { /* The light is visible somewhere */
-//			System.out.println("Found the light!");
-//			return true;
-//		} else { /* There's no light; let's clear our path so we don't try to draw stuff */
-//			this.pathToLight = null;
-//			return false;
-//		}
-		
-		return true;
+		if(!ctx.objects.select().id(91665).isEmpty()) { /* The light is visible somewhere */
+			System.out.println("Found the light!");
+			return true;
+		} else { /* There's no light; let's clear our path so we don't try to draw stuff */
+			this.pathToLight = null;
+			return false;
+		}
 	}
 
 	@Override
 	public void execute() {
 		/* Set up our path */
-//		GameObject light = ctx.objects.select().id(this.groundLightId).poll();
-//		this.pathToLight = ctx.movement.findPath(light);
+		GameObject light = ctx.objects.select().id(this.groundLightId).poll();
+		this.pathToLight = ctx.movement.findPath(light);
 	}
 
 	@Override
 	public void repaint(Graphics g) {
 		g.setColor(Color.CYAN);
-		g.drawPolygon(ctx.players.local().tile().matrix(ctx).bounds());
-		Point mapPoint = ctx.players.local().tile().matrix(ctx).mapPoint();
-		g.drawRect((int)mapPoint.getX() - 2, (int)mapPoint.getY() - 2, 4, 4);
-//		if(this.pathToLight != null) {
-//			Tile nextTile = this.pathToLight.next();
-//			while(nextTile != this.pathToLight.end()) { /* Loop through our path until we get to the end */
-//				g.drawPolygon(nextTile.matrix(ctx).bounds());
-//				nextTile = this.pathToLight.next();
-//			}
-//		}
+		
+		if(this.pathToLight != null) {
+			Tile nextTile = this.pathToLight.next();
+			while(nextTile != this.pathToLight.end()) { /* Loop through our path until we get to the end */
+				Point mapPoint = nextTile.matrix(ctx).mapPoint();
+				g.drawPolygon(nextTile.matrix(ctx).bounds()); // Draw the polygon on screen
+				g.drawRect((int)mapPoint.getX() - 2, (int)mapPoint.getY() - 2, 4, 4); // Draw the point on the map
+				nextTile = this.pathToLight.next();
+			}
+		}
 	}
 
 }
